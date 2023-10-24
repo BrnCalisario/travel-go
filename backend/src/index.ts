a00cb37f9e08c5f0a00c433834f266bf0e652b7b
BrnCalisario <bruno145224@gmail.com>
2023-10-24 15:44:47 -0300

    setting up db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express, { Router, Request, Response } from "express"
+import cors from "cors"
+
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 3000
-
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req : Request, res : Response) => {
     res.json({ message: 'Hello World' })
