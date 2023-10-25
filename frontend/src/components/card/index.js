80647655dd90fe754c10da6e311627074a3a972b
Murilll <murilosertorio25@gmail.com>
2023-10-25 15:48:34 -0300

    create: card and components

---
+++ frontend/src/components/card/index.js
@@ -0,0 +1,12 @@
+import styles from "./styles";
+import { View, Image } from "react-native";
+
+export default function CardComp() {
+    return (
+        <>
+            <View style={styles.card}>
+                <Image />
+            </View>
+        </>
+    )
+}
---
+++ frontend/src/components/card/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'red',
+    },
+});
+
+export default styles;
