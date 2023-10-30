9447fe322662cd62959778466059afba0c836e28
Murilll <murilosertorio25@gmail.com>
2023-10-30 16:18:51 -0300

    create: page of update account

--- frontend/src/components/logo/styles.js
+++ frontend/src/components/logo/styles.js
@@ -3,12 +3,11 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     component: {
         alignItems: 'center',
-        height: '5rem'
     },
     logo: {
         display: 'flex',
         flexDirection: 'row',
-        height: '3rem'
+        
     },
     firstSyllable: {
         color: '#1971c2',
--- frontend/src/pages/account/index.js
+++ frontend/src/pages/account/index.js
@@ -1,8 +1,53 @@
+import { View, TextInput, Button } from "react-native";
+import NavBar from "../../components/navbar";
+import CustomTextLOS from "../../components/CustomTextLOS";
+import styles from "./styles";
 
 export default function AccountPage() {
     return (
         <>
-            
+            <NavBar />
+            <View style={styles.screen}>
+                <View style={styles.componentLogin}>
+                    <CustomTextLOS>Email</CustomTextLOS>
+                    <TextInput
+                        onchangeText={e => setEmail(e)}
+                        style={styles.input}
+                    />
+                </View>
+            </View>
+            <View style={styles.screen}>
+                <View style={styles.componentLogin}>
+                    <CustomTextLOS>Name</CustomTextLOS>
+                    <TextInput
+                        onchangeText={e => setEmail(e)}
+                        style={styles.input}
+                    />
+                </View>
+            </View>
+            <View style={styles.screen}>
+                <View style={styles.componentLogin}>
+                    <CustomTextLOS>CPF</CustomTextLOS>
+                    <TextInput
+                        onchangeText={e => setEmail(e)}
+                        style={styles.input}
+                    />
+                </View>
+            </View>
+            <View style={styles.screen}>
+                <View style={styles.componentLogin}>
+                    <CustomTextLOS>Password</CustomTextLOS>
+                    <TextInput
+                        onchangeText={e => setEmail(e)}
+                        style={styles.input}
+                    />
+                </View>
+            </View>
+            <View style={styles.button}>
+                <Button
+                    title="Update"
+                    color="#006EE4" />
+            </View>
         </>
     );
 }
