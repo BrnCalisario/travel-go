486b1b386fe1d4bdc06990eb03b7c1c29a34c5ae
LucaFreire <lucasrodifreire@gmail.com>
2024-02-26 15:34:28 -0300

    add: payment's style

--- frontend/src/pages/payment/index.js
+++ frontend/src/pages/payment/index.js
@@ -1,20 +1,27 @@
 import { View, Button, Text } from "react-native";
-import CustomTextLOS from "../../components/CustomTextLOS";
 import style from "./style";
+import { FcMoneyTransfer } from "react-icons/fc";
+import { TouchableOpacity } from "react-native-web";
 
 export default function Payment(props) {
-
-
 
 
     return (
         <View style={style.main}>
 
-            <Text>Payment</Text>
+            <View style={style.header}>
+                <Text style={style.title}>Pedido Finalizado!</Text>
+            </View>
+
+            <View style={style.paymentMethod}>
+                <FcMoneyTransfer style={style.paymentIcon}/>
+                <Text style={style.paymentMethodText}>Escolha sua forma de pagamento:</Text>
+            </View>
+
 
             <View style={style.buttonSection}>
-                <Button title="PIX">PIX</Button>
-                <Button title="Boleto">Boleto</Button>
+                <TouchableOpacity style={style.payButton} title="Boleto">Boleto</TouchableOpacity>
+                <TouchableOpacity style={style.payButton} title="PIX">PIX</TouchableOpacity>
             </View>
 
         </View>
--- frontend/src/pages/payment/style.js
+++ frontend/src/pages/payment/style.js
@@ -6,13 +6,51 @@ const styles = StyleSheet.create({
         height: '100vh',
         color: 'gray',
         alignItems: 'center',
-        justifyContent: 'center',
         display: 'flex',
         flexDirection: 'column'
     },
     buttonSection: {
         display: 'flex',
-        gap: '20px'
+        // flexDirection: 'row',
+        gap: '40px',
+        height: "30%",
+        width: "100%",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    payButton: {
+        width: '30%',
+        justifyContent:'center',
+        alignItems: 'center',
+        color: 'white',
+        padding: '6px',
+        borderRadius: '10px',
+        backgroundColor: 'rgb(9, 106, 58)'
+    },
+    title: {
+        fontSize: '40px',
+        color: 'rgb(87, 222, 149)'
+    },
+    header: {
+        height: "40%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    paymentMethod: {
+        width: "100%",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: "center",
+        height: "30%",
+        gap: "30px"
+    },
+    paymentIcon: {
+        fontSize: '200px'
+    },
+    paymentMethodText: {
+        fontSize: '18px'
     }
 
 
